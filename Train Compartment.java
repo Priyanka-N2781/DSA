6a31c1f89dcb5b6951dedd6e5cb8a818f180695f
Priyanka N <priyanka.n2781@gmail.com>
2026-09-18 18:03:41 +0530

    Update Train Compartment.java

--- Train Compartment.java
+++ Train Compartment.java
@@ -1,80 +1,57 @@
-import java.util.Scanner;
+import java.util.*;
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int val) { this.val = val; }
-}
-
-public class Main {
-    public static ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null || k == 1) {
-            return head;
-        }
-
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode groupPrev = dummy;
-
-        while (true) {
-            ListNode kth = groupPrev;
-            for (int i = 0; i < k; i++) {
-                kth = kth.next;
-                if (kth == null) {
-                    return dummy.next;
-                }
-            }
-
-            ListNode groupNext = kth.next;
-            ListNode prev = groupNext; 
-            ListNode curr = groupPrev.next;
-
-            while (curr != groupNext) {
-                ListNode tmp = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = tmp;
-            }
-
-            ListNode tmp = groupPrev.next;
-            groupPrev.next = kth;
-            groupPrev = tmp;
+class Main {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
         }
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-        if (n <= 0) {
-            System.out.println("[]");
-            scanner.close();
-            return;
-        }
-
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        for (int i = 0; i < n; i++) {
-            current.next = new ListNode(scanner.nextInt());
-            current = current.next;
-        }
-        ListNode head = dummy.next;
-
-        int k = scanner.nextInt();
-
-        ListNode resultHead = reverseKGroup(head, k);
-
-        System.out.print("[");
-        ListNode temp = resultHead;
-        while (temp != null) {
-            System.out.print(temp.val);
-            if (temp.next != null) {
-                System.out.print(", ");
-            }
+    static Node reverseK(Node head, int k) {
+        Node current = head;
+        Node prev = null;
+        Node next = null;
+        Node temp = head;
+        for (int i = 0; i < k; i++) {
+            if (temp == null)
+                return head;
             temp = temp.next;
         }
-        System.out.println("]");
+        
+        for (int i = 0; i < k; i++) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
 
-        scanner.close();
+        head.next = reverseK(current, k);
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Node head = null;
+        Node tail = null;
+        for (int i = 0; i < n; i++) {
+            Node newNode = new Node(sc.nextInt());
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        int k = sc.nextInt();
+        head = reverseK(head, k);
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
     }
 }
